8fa26f08ff54ef6f1a77cf296443a3d1a526f8ea
umashankar <umashankarp33@gmail.com>
2025-07-14 12:46:25 +0530

    add auth logic

---
+++ brainly-backend/src/index.js
@@ -0,0 +1,8 @@
+import http from 'http'
+import app from './app.js';
+let server=http.createServer(app)
+let PORT=5000;
+
+server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT} 🚀...`);
+})
