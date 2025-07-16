c7b288236d1117a52321286edcd7ab37dad70eb0
umashankar <umashankarp33@gmail.com>
2025-07-16 12:20:24 +0530

    add user verification using token auth middleware

--- brainly-backend/src/controllers/profile.controllers.js
+++ brainly-backend/src/controllers/profile.controllers.js
@@ -1,3 +1,6 @@
-export let getUserProfile=(req,res,next)=>{
-    res.send("user profile")
+import User from "../models/user.model.js"
+
+export let getUserProfile=async (req,res,next)=>{
+    let user=await User.findById(req.userId)
+    res.send(`hello ${user.username}`)
 }
--- brainly-backend/src/middlewares/auth.js
+++ brainly-backend/src/middlewares/auth.js
@@ -1,14 +1,18 @@
 import jwt from 'jsonwebtoken'
+import User from '../models/user.model.js';
 
 
-export let auth=(req,res,next)=>{
+export let auth=async (req,res,next)=>{
     let token=req.headers?.authorization?.split(" ")[1];
     if(!token){
-        return res.status(401).send("Unauthenticated")
+        return res.status(401).send("Please Login")
     }
     let decodedToken=jwt.verify(token,"topSecret")
-    console.log(decodedToken);
-    
+    let user=await User.findById(decodedToken.id)
+    if(!user){
+        return res.status(401).send("There is no such user please register")
+    }
+    req.userId=user._id
     next()
 }
 
