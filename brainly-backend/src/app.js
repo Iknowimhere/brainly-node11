865f264688db70e8d0753ac2752ce4e791a899f4
umashankar <umashankarp33@gmail.com>
2025-07-25 12:36:37 +0530

    protect backend

--- brainly-backend/src/app.js
+++ brainly-backend/src/app.js
@@ -5,13 +5,30 @@ import db from "./config/db.js";
 import userRoutes from './routes/user.routes.js'
 import contentRoutes from './routes/content.route.js'
 import brainRoutes from './routes/share.route.js'
+import {rateLimit} from 'express-rate-limit'
+import morgan from 'morgan'
 import cors from 'cors'
+import helmet from 'helmet'
 let app = express();
 db()
 
-
-
 //middleware
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+}))
+app.use(rateLimit({
+    windowMs:30*1000,
+    limit:10,
+    standardHeaders:true,
+    legacyHeaders:false
+}))
+app.use(helmet())
 app.use(cors())
 app.use(express.json());
 
--- brainly-backend/src/index.js
+++ brainly-backend/src/index.js
@@ -1,7 +1,33 @@
-import http from 'http'
+import http from 'http';
+import https from 'https';
 import app from './app.js';
-let server=http.createServer(app)
-let PORT=process.env.PORT || 5000;
+let PORT = process.env.PORT || 5000;
+
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let server;
+try {
+  const keyPath = path.resolve(__dirname, '../server.key');
+  const certPath = path.resolve(__dirname, '../server.crt');
+  const serverOptions = {
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath)
+  };
+  server = https.createServer(serverOptions, app);
+  console.log('HTTPS server started.');
+} catch (err) {
+  console.error('HTTPS error:', err.message);
+  console.warn('HTTPS certs not found or invalid, falling back to HTTP.');
+  server = http.createServer(app);
+}
+
+
+
 
 server.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT} 🚀...`);
